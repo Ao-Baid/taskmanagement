25c410eb734145a5f11baf2ca4c2900368351258
Ao-baid <aobaid2004@outlook.com>
2023-03-01 20:33:14 +0000

    Removed subtasks and tasks from admin view

--- taskmanage/admin.py
+++ taskmanage/admin.py
@@ -7,28 +7,9 @@ from django.contrib.admin.models import LogEntry
 
 
 #in the log entry table, allow the rows to show multiple fields
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('get_user_id','task_name', 'task_description', 'task_status', 'task_created_at', 'task_updated_at')
-
-    @admin.display(description='User ID')
-    def get_user_id(self, obj):
-        return obj.user_id
-
-admin.site.register(Task, TaskAdmin)
-
-class SubTaskAdmin(admin.ModelAdmin):
-    list_display = ('get_task', 'subtask_name', 'subtask_description', 'subtask_status', 'subtask_created_at', 'subtask_updated_at', 'get_task')
-
-    @admin.display(description='Task')
-    def get_task(self, obj):
-        return obj.task
-
-    @admin.display(description='Task')
-    def get_task(self, obj):
-        return obj.task
 
 
-admin.site.register(SubTask, SubTaskAdmin)
+
 
 #in the log entry table, allow the rows to show multiple fields
 class LogEntryAdmin(admin.ModelAdmin):
